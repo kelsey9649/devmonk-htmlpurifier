9ac17394ebafd3d84d8f2278fed07d4cf51b3985
Curtis Kelsey <kelseyc@truman.edu>
2017-03-20 07:40:38 -0500

    Added licensing

--- tests/Bootstrap.php
+++ tests/Bootstrap.php
@@ -1,4 +1,21 @@
 <?php
+/**
+ * DevmonkHtmlpurifier Copyright (C) 2013-2017 Peter Aba
+ * Contributors: Curtis Kelsey <curtis.kelsey@gmail.com>
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
 use Zend\Mvc\Application;
 chdir(__DIR__ .'/../'); // chdir() to application root
 require 'vendor/autoload.php';
