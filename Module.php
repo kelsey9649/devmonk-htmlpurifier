f5400903dd7624a1e04808aa54351f83324378af
Curtis Kelsey <curtis.kelsey@gmail.com>
2017-06-16 08:15:19 -0500

    Updated the old Module.php
    Updated the old Module.php to point at the PSR-4 one to provide backwards compatibility

--- Module.php
+++ Module.php
@@ -1,20 +1,3 @@
 <?php
 
-namespace DevmonkHtmlpurifier;
-
-use Zend\EventManager\EventInterface;
-
-class Module
-{
-    const SERVICE_NAME            = 'purifier';
-    const CONFIG_KEY_HTMLPURIFIER = 'devmonkhtmlpurifier';
-    const CONFIG_KEY_CONFIG       = 'config';
-
-    /**
-     * {@inheritDoc}
-     */
-    public function getConfig()
-    {
-        return include __DIR__ . '/config/module.config.php';
-    }
-}
+include __DIR__ . '/src/DevmonkHtmlpurifier/Module.php';
