bafdca66d06867c67fc2f882a1a61aed1b80d2fa
Curtis Kelsey <kelseyc@truman.edu>
2017-06-16 08:01:42 -0500

    Updates for Zend Framework 3 support

--- src/DevmonkHtmlpurifier/Service/HtmlPurifierFactory.php
+++ src/DevmonkHtmlpurifier/Service/HtmlPurifierFactory.php
@@ -3,6 +3,10 @@
 namespace DevmonkHtmlpurifier\Service;
 
 use DevmonkHtmlpurifier\Module;
+use Interop\Container\ContainerInterface;
+use Interop\Container\Exception\ContainerException;
+use Zend\ServiceManager\Exception\ServiceNotCreatedException;
+use Zend\ServiceManager\Exception\ServiceNotFoundException;
 use Zend\ServiceManager\FactoryInterface;
 use Zend\ServiceManager\ServiceLocatorInterface;
 
@@ -20,7 +24,24 @@ class HtmlPurifierFactory implements FactoryInterface
      */
     public function createService(ServiceLocatorInterface $services)
     {
-        $config = $services->get('Config');
+        return $this->__invoke($services, \HTMLPurifier::class);
+    }
+
+    /**
+     * Create an object
+     *
+     * @param  ContainerInterface $container
+     * @param  string $requestedName
+     * @param  null|array $options
+     * @return \HTMLPurifier
+     * @throws ServiceNotFoundException if unable to resolve the service.
+     * @throws ServiceNotCreatedException if an exception is raised when
+     *     creating a service.
+     * @throws ContainerException if any other error occurs
+     */
+    public function __invoke(ContainerInterface $container, $requestedName, array $options = null)
+    {
+        $config = $container->get('Config');
 
         $purifierConfig = \HTMLPurifier_Config::createDefault();
 
